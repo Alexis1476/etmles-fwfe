233fa3029731af8bf358814ce1ca1a57f26bc14d
Alexis1476 <halexisr20@hotmail.com>
2026-02-12 10:35:01 +0100

    Finish ex 5

--- src/exercises/04-StarWars.vue
+++ src/exercises/04-StarWars.vue
@@ -9,12 +9,12 @@ const characters = ref([
     image: "https://preview.redd.it/who-was-the-most-powerful-jedi-in-your-opinion-v0-0o43kmihipee1.jpg?width=640&crop=smart&auto=webp&s=273293e3413f0d3f88ac064b6bf1122d82aa8c78"
   },
   {
-    id: 1,
+    id: 2,
     name: "Wookie",
     image: "https://upload.wikimedia.org/wikipedia/commons/d/d7/SWC4_-_Costume_Pageant-_Chewbacca_%28518145361%29.jpg"
   },
   {
-    id: 1,
+    id: 3,
     name: "Droide",
     image: "https://upload.wikimedia.org/wikipedia/en/thumb/3/37/R2-D2_%26_C-3PO_%28Star_Wars%29.jpg/250px-R2-D2_%26_C-3PO_%28Star_Wars%29.jpg"
   },
--- src/exercises/05-StarWars2.vue
+++ src/exercises/05-StarWars2.vue
@@ -2,45 +2,89 @@
 import ExerciseBase from '../components/ExerciseBase.vue'
 import {ref} from "vue";
 
-const characters = ref([
+const characters = [
   {
     id: 1,
     name: "Jedi",
-    image: "https://preview.redd.it/who-was-the-most-powerful-jedi-in-your-opinion-v0-0o43kmihipee1.jpg?width=640&crop=smart&auto=webp&s=273293e3413f0d3f88ac064b6bf1122d82aa8c78"
+    style: {
+      background: 2,
+      color: "green"
+    },
+    image: "https://preview.redd.it/who-was-the-most-powerful-jedi-in-your-opinion-v0-0o43kmihipee1.jpg?width=640&crop=smart&auto=webp&s=273293e3413f0d3f88ac064b6bf1122d82aa8c78",
   },
   {
-    id: 1,
+    id: 2,
     name: "Wookie",
-    image: "https://upload.wikimedia.org/wikipedia/commons/d/d7/SWC4_-_Costume_Pageant-_Chewbacca_%28518145361%29.jpg"
+    image: "https://upload.wikimedia.org/wikipedia/commons/d/d7/SWC4_-_Costume_Pageant-_Chewbacca_%28518145361%29.jpg",
   },
   {
-    id: 1,
+    id: 3,
     name: "Droide",
-    image: "https://upload.wikimedia.org/wikipedia/en/thumb/3/37/R2-D2_%26_C-3PO_%28Star_Wars%29.jpg/250px-R2-D2_%26_C-3PO_%28Star_Wars%29.jpg"
+    image: "https://upload.wikimedia.org/wikipedia/en/thumb/3/37/R2-D2_%26_C-3PO_%28Star_Wars%29.jpg/250px-R2-D2_%26_C-3PO_%28Star_Wars%29.jpg",
   },
-])
+]
+
+const characterSelected = ref(characters[0]);
+const changeCharacter = (char) => {
+  characterSelected.value = char
+}
 </script>
 
 <template>
   <ExerciseBase title="Exercise 5">
     <h2>Personnages de StarWars</h2>
-    <ul>
-      <li v-for="character in characters" :key="character.id">{{ character.name }}</li>
-    </ul>
-    <ul class="characters-container">
-      <figure v-for="character in characters" :key="character.id">
-        <img :src="character.image" :alt="character.name" height="150" width="110" />
-        <figcaption>{{character.name}}</figcaption>
+    <div class="characters-container">
+      <figure :key="characterSelected.id">
+        <img :alt="characterSelected.name" :src="characterSelected.image" height="150"/>
       </figure>
-    </ul>
+
+      <button v-for="char in characters" :key="char.id"
+              :class="['btn', `${char.name.toLowerCase()}-btn`, { 'selected-character': char.id === characterSelected.id }]"
+              @mouseenter="changeCharacter(char)">
+        {{ char.name }}
+      </button>
+    </div>
   </ExerciseBase>
 </template>
 
 <style scoped>
-.characters-container{
+.btn.selected-character, .btn:hover {
+  background-color: red;
+}
+
+.btn {
+  border: none;
+  padding: 8px;
+}
+
+.jedi-btn {
+  background: lightgray;
+}
+
+.wookie-btn {
+  background: lightskyblue;
+}
+
+.droide-btn {
+  background: yellow;
+}
+
+.characters-container {
   margin-top: 2em;
   display: flex;
-  flex-direction: row;
-  justify-content: space-between;
+  flex-direction: column; /* Aligne tout en colonne */
+  width: 10em;
+  gap: 5px;
+}
+
+img {
+  width: 100%;
+  height: 10em;
+}
+
+.btn {
+  width: 100%;
+  padding: 5px;
+  cursor: pointer;
 }
 </style>
